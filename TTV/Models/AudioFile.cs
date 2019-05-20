493a98f59dc46b847b9e656fca84c7f9953c4d7e
Alex Waychoff <alexwaychoff@gmail.com>
2019-05-20 13:07:18 -0500

    added AudioFile model, will be used for code first migrations

---
+++ TTV/Models/AudioFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TTV.Models
+{
+    public class AudioFile
+    {
+        public int audioID { get; set; }
+
+        public string fileName { get; set; }
+
+        public Nullable<int> fileSize { get; set; }
+
+        public string filePath { get; set; }
+    }
+}
